c25dab0b36317c1b56766b0c41c3eb44fc9c66c0
zab88 <alexey.zabaykin@gmail.com>
2017-11-12 15:24:09 +0700

    4 types extraction

--- ruwiktionary/parser.py
+++ ruwiktionary/parser.py
@@ -1,45 +1,46 @@
 import re
 
 
-ii = 0
 page_started = False
 current_word = None
-synonyms_started = False
-current_synonyms = ''
+
+connection_types = ['Синонимы', 'Антонимы', 'Гиперонимы', 'Гипонимы']
+connection_labels = ['==== {} ===='.format(x) for x in connection_types]
+current_connection_words = ['', '', '', '']
+type_started = None
 
 def brackets_del(s):
     reg = re.compile('\[\[[^\]]+\]\]')
     words = reg.findall(s)
-    return 'synonyms:' + ', '.join([x.replace('[', '').replace(']', '') for x in words])
+    return ', '.join([x.replace('[', '').replace(']', '') for x in words])
 
 with open('../../graph/ruwiktionary-20171020-pages-articles-multistream.xml', 'r', encoding='utf-8') as f:
     for line in f:
         ll = line.strip()
         if '<page>' in line.strip():
-            if current_word and current_synonyms:
+            if current_word and any(x for x in current_connection_words):
                 print(current_word)
-                print(brackets_del(current_synonyms))
+                for k, el in enumerate(current_connection_words):
+                    if not brackets_del(el):
+                        continue
+                    print(connection_types[k], brackets_del(el))
 
             page_started = True
             current_word = None
-            current_synonyms = ''
+            current_connection_words = ['', '', '', '']
+            type_started = None
         if '</page>' in line.strip():
             page_started = False
 
         if page_started and '<title>' in ll and '</title>' in ll:
             current_word = ll.replace('<title>', '').replace('</title>', '')
 
-        if current_word is not None and '==== Синонимы ====' in ll:
-            synonyms_started = True
+        if current_word is not None and ll in connection_labels:
+            type_started = connection_labels.index(ll)
             continue
 
-        if synonyms_started and '====' in ll:
-            synonyms_started = False
+        if type_started is not None and '====' in ll:
+            type_started = None
 
-        if synonyms_started:
-            current_synonyms += ll
-
-        # ii += 1
-        # print(line)
-        # if ii > 100:
-        #     break
+        if type_started is not None:
+            current_connection_words[type_started] += ll
